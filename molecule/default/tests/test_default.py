199e6e43ba04e4752fc129829f46a08b30aeca08
daniel-wtd <daniel@while-true-do.io>
2019-05-15 19:05:51 +0200

    fix: testinfra failing

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,14 +7,26 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_epel_file(host):
-    file = host.file('/etc/yum.repos.d/epel.repo')
-    assert file.exists
-    assert file.user == 'root'
-    assert file.group == 'root'
+    if host.system_info.distribution == "CentOS":
+        file = host.file('/etc/yum.repos.d/epel.repo')
+        assert file.exists
+        assert file.user == 'root'
+        assert file.group == 'root'
+    if host.system_info.distribution == "RedHat":
+        file = host.file('/etc/yum.repos.d/epel.repo')
+        assert file.exists
+        assert file.user == 'root'
+        assert file.group == 'root'
 
 
 def test_epel_testing_file(host):
-    file = host.file('/etc/yum.repos.d/epel-testing.repo')
-    assert file.exists
-    assert file.user == 'root'
-    assert file.group == 'root'
+    if host.system_info.distribution == "CentOS"
+        file = host.file('/etc/yum.repos.d/epel-testing.repo')
+        assert file.exists
+        assert file.user == 'root'
+        assert file.group == 'root'
+    if host.system_info.distribution == "RedHat":
+        file = host.file('/etc/yum.repos.d/epel.repo')
+        assert file.exists
+        assert file.user == 'root'
+        assert file.group == 'root'
